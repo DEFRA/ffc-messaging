90497d74903e8f40efedc0a2db6544f9d705fe15
John Watson <johnwatson484@hotmail.com>
2020-11-04 12:21:29 +0000

    Subject optional

--- app/message-schema.js
+++ app/message-schema.js
@@ -2,7 +2,7 @@ const Joi = require('joi')
 
 module.exports = Joi.object({
   body: Joi.object().allow(null),
-  subject: Joi.string().required(),
+  subject: Joi.string().allow(''),
   type: Joi.string().required(),
   source: Joi.string().required()
 })
