c8adfc7a8279858c71a8fdb625408ce43b7d500a
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-08-04 20:43:15 +0100

    Add bulk message option

--- app/messaging/index.js
+++ app/messaging/index.js
@@ -1,7 +1,9 @@
 const MessageReceiver = require('./message-receiver')
 const MessageSender = require('./message-sender')
+const BulkMessageSender = require('./bulk-message-sender')
 
 module.exports = {
   MessageReceiver,
-  MessageSender
+  MessageSender,
+  BulkMessageSender
 }
---
+++ app/messaging/message-bulk-sender.js
@@ -0,0 +1,56 @@
+const MessageBase = require('./message-base')
+const messageSchema = require('./message-schema')
+const retry = require('../retry')
+const { trackTrace } = require('../app-insights')
+
+class BulkMessageSender extends MessageBase {
+  constructor (config) {
+    super(config)
+    this.sendBatchMessages = this.sendBatchMessages.bind(this)
+    this.sender = this.sbClient.createSender(config.address)
+  }
+
+  async sendBatchMessages (messages, options = {}) {
+    let batch = await this.sender.createMessageBatch()
+
+    for (const message of messages) {
+      await messageSchema.validateAsync(message, { allowUnknown: true })
+      const enrichMessage = this.enrichMessage(message)
+      trackTrace(this.appInsights, this.connectionName)
+
+      if (!batch.tryAddMessage(enrichMessage)) {
+        await retry(() => this.send(batch, options), this.config.retries, this.config.retryWaitInMs, this.config.exponentialRetry)
+        batch = await this.sender.createMessageBatch()
+
+        if (!batch.tryAddMessage(enrichMessage)) {
+          throw new Error('Message too big to fit in a batch')
+        }
+      }
+    }
+
+    await retry(() => this.send(batch, options), this.config.retries, this.config.retryWaitInMs, this.config.exponentialRetry)
+
+    return batch
+  }
+
+  async send (message, options) {
+    await this.sender.sendMessages(message, options)
+  }
+
+  async closeConnection () {
+    await this.sender.close()
+    await super.closeConnection()
+  }
+
+  enrichMessage (message) {
+    return {
+      ...message,
+      applicationProperties: {
+        type: message.type,
+        source: message.source
+      }
+    }
+  }
+}
+
+module.exports = BulkMessageSender
