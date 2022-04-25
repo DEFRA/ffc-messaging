f9cc295732503516b401de7cecfd627a43f7c579
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-04-25 14:05:55 +0100

    Add constructor

--- app/messaging/subscription-properties.js
+++ app/messaging/subscription-properties.js
@@ -1,6 +1,11 @@
 const AdministrationBase = require('./administration-base')
 
 class SubscriptionProperties extends AdministrationBase {
+  constructor (config) {
+    super(config)
+    this.getSubscriptionDetails = this.getSubscriptionDetails.bind(this)
+  }
+
   async getSubscriptionDetails (topicName, subscriptionName) {
     const subscriptionProperties = await this.sbAdministrationClient.getSubscriptionRuntimeProperties(topicName, subscriptionName)
     return { activeMessageCount: subscriptionProperties.activeMessageCount, deadLetterMessageCount: subscriptionProperties.deadLetterMessageCount }
