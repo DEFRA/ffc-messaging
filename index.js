1a6eb914c872caa07f01900b3098b34854542c71
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-04-25 14:17:32 +0100

    Added SubscriptionProperties

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const appInsights = require('./app/app-insights')
-const { MessageReceiver, MessageSender, MessageBatchSender } = require('./app/messaging')
+const { MessageReceiver, MessageSender, MessageBatchSender, SubscriptionProperties } = require('./app/messaging')
 
 module.exports = {
   MessageSender,
   MessageReceiver,
   MessageBatchSender,
+  SubscriptionProperties,
   appInsights
 }
