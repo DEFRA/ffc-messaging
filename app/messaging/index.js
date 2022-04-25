741ae763af58c58b33290150a53104e6201c72b8
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-04-25 14:12:33 +0100

    Add SubscriptionProperties

--- app/messaging/index.js
+++ app/messaging/index.js
@@ -1,9 +1,11 @@
 const MessageReceiver = require('./message-receiver')
 const MessageSender = require('./message-sender')
 const MessageBatchSender = require('./message-batch-sender')
+const SubscriptionProperties = require('./subscription-properties')
 
 module.exports = {
   MessageReceiver,
   MessageSender,
-  MessageBatchSender
+  MessageBatchSender,
+  SubscriptionProperties
 }
