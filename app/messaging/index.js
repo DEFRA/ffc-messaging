ec5d3b7b364a23331b61313cd73d53ac5124d638
John Watson <johnwatson484@hotmail.com>
2022-11-14 19:24:46 +0000

    Add client reference

--- app/messaging/index.js
+++ app/messaging/index.js
@@ -1,9 +1,11 @@
 const MessageReceiver = require('./message-receiver')
 const MessageSender = require('./message-sender')
 const MessageBatchSender = require('./message-batch-sender')
+const AdminClient = require('./admin-client')
 
 module.exports = {
   MessageReceiver,
   MessageSender,
-  MessageBatchSender
+  MessageBatchSender,
+  AdminClient
 }
